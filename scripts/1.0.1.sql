963718706873708f20b6d5df14e8983de4e1032a
Fernando Araujo <developgadget@gmail.com>
2024-11-07 22:31:09 -0500

    add currency

---
+++ scripts/1.0.1.sql
@@ -0,0 +1,1218 @@
+CREATE TABLE
+    currency (name TEXT, code TEXT, PRIMARY KEY (name, code));
+
+-
+CREATE TABLE
+    symbol_currency (name TEXT, code TEXT, PRIMARY KEY (name, code));
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('US Dollar', 'USD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Canadian Dollar', 'CAD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Euro', 'EUR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('United Arab Emirates Dirham', 'AED');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Afghan Afghani', 'AFN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Albanian Lek', 'ALL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Armenian Dram', 'AMD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Argentine Peso', 'ARS');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Australian Dollar', 'AUD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Azerbaijani Manat', 'AZN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Bosnia-Herzegovina Convertible Mark', 'BAM');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Bangladeshi Taka', 'BDT');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Bulgarian Lev', 'BGN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Bahraini Dinar', 'BHD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Burundian Franc', 'BIF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Brunei Dollar', 'BND');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Bolivian Boliviano', 'BOB');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Brazilian Real', 'BRL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Botswanan Pula', 'BWP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Belarusian Ruble', 'BYR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Belize Dollar', 'BZD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Congolese Franc', 'CDF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Swiss Franc', 'CHF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Chilean Peso', 'CLP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Chinese Yuan', 'CNY');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Colombian Peso', 'COP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Costa Rican Colón', 'CRC');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Cape Verdean Escudo', 'CVE');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Czech Republic Koruna', 'CZK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Djiboutian Franc', 'DJF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Danish Krone', 'DKK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Dominican Peso', 'DOP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Algerian Dinar', 'DZD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Estonian Kroon', 'EEK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Egyptian Pound', 'EGP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Eritrean Nakfa', 'ERN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Ethiopian Birr', 'ETB');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('British Pound Sterling', 'GBP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Georgian Lari', 'GEL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Ghanaian Cedi', 'GHS');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Guinean Franc', 'GNF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Guatemalan Quetzal', 'GTQ');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Hong Kong Dollar', 'HKD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Honduran Lempira', 'HNL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Croatian Kuna', 'HRK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Hungarian Forint', 'HUF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Indonesian Rupiah', 'IDR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Israeli New Sheqel', 'ILS');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Indian Rupee', 'INR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Iraqi Dinar', 'IQD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Iranian Rial', 'IRR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Icelandic Króna', 'ISK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Jamaican Dollar', 'JMD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Jordanian Dinar', 'JOD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Japanese Yen', 'JPY');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Kenyan Shilling', 'KES');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Cambodian Riel', 'KHR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Comorian Franc', 'KMF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('South Korean Won', 'KRW');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Kuwaiti Dinar', 'KWD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Kazakhstani Tenge', 'KZT');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Lebanese Pound', 'LBP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Sri Lankan Rupee', 'LKR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Lithuanian Litas', 'LTL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Latvian Lats', 'LVL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Libyan Dinar', 'LYD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Moroccan Dirham', 'MAD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Moldovan Leu', 'MDL');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Malagasy Ariary', 'MGA');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Macedonian Denar', 'MKD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Myanma Kyat', 'MMK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Macanese Pataca', 'MOP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Mauritian Rupee', 'MUR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Mexican Peso', 'MXN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Malaysian Ringgit', 'MYR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Mozambican Metical', 'MZN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Namibian Dollar', 'NAD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Nigerian Naira', 'NGN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Nicaraguan Córdoba', 'NIO');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Norwegian Krone', 'NOK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Nepalese Rupee', 'NPR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('New Zealand Dollar', 'NZD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Omani Rial', 'OMR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Panamanian Balboa', 'PAB');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Peruvian Nuevo Sol', 'PEN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Philippine Peso', 'PHP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Pakistani Rupee', 'PKR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Polish Zloty', 'PLN');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Paraguayan Guarani', 'PYG');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Qatari Rial', 'QAR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Romanian Leu', 'RON');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Serbian Dinar', 'RSD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Russian Ruble', 'RUB');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Rwandan Franc', 'RWF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Saudi Riyal', 'SAR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Sudanese Pound', 'SDG');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Swedish Krona', 'SEK');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Singapore Dollar', 'SGD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Somali Shilling', 'SOS');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Syrian Pound', 'SYP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Thai Baht', 'THB');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Tunisian Dinar', 'TND');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Tongan Paʻanga', 'TOP');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Turkish Lira', 'TRY');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Trinidad and Tobago Dollar', 'TTD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('New Taiwan Dollar', 'TWD');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Tanzanian Shilling', 'TZS');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Ukrainian Hryvnia', 'UAH');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Ugandan Shilling', 'UGX');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Uruguayan Peso', 'UYU');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Uzbekistan Som', 'UZS');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Venezuelan Bolívar', 'VEF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Vietnamese Dong', 'VND');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('CFA Franc BEAC', 'XAF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('CFA Franc BCEAO', 'XOF');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Yemeni Rial', 'YER');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('South African Rand', 'ZAR');
+
+-
+INSERT INTO
+    currency (name, code)
+VALUES
+    ('Zambian Kwacha', 'ZMK');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('US Dollar', '$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Euro', '€');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Afghan Afghani', '؋');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Albanian Lek', 'Lek');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Armenian Dram', 'դր.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Azerbaijani Manat', 'ман.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Bosnia-Herzegovina Convertible Mark', 'KM');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Bangladeshi Taka', '৳');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Bulgarian Lev', 'лв.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Burundian Franc', 'FBu');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Bolivian Boliviano', 'Bs');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Brazilian Real', 'R$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Botswanan Pula', 'P');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Belarusian Ruble', 'BYR');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Congolese Franc', 'FrCD');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Swiss Franc', 'CHF');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Chinese Yuan', 'CN¥');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Costa Rican Colón', '₡');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Cape Verdean Escudo', 'CV$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Czech Republic Koruna', 'Kč');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Djiboutian Franc', 'Fdj');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Danish Krone', 'kr');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Dominican Peso', 'RD$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Eritrean Nakfa', 'Nfk');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Ethiopian Birr', 'Br');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('British Pound Sterling', '£');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Georgian Lari', 'GEL');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Ghanaian Cedi', 'GH₵');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Guinean Franc', 'FG');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Guatemalan Quetzal', 'Q');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Honduran Lempira', 'L');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Croatian Kuna', 'kn');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Hungarian Forint', 'Ft');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Indonesian Rupiah', 'Rp');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Israeli New Sheqel', '₪');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Indian Rupee', 'টকা');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Iranian Rial', '﷼');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Japanese Yen', '￥');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Kenyan Shilling', 'Ksh');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Cambodian Riel', '៛');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Comorian Franc', 'FC');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('South Korean Won', '₩');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Kazakhstani Tenge', 'тңг.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Sri Lankan Rupee', 'SL Re');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Lithuanian Litas', 'Lt');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Latvian Lats', 'Ls');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Moldovan Leu', 'MDL');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Malagasy Ariary', 'MGA');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Macedonian Denar', 'MKD');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Myanma Kyat', 'K');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Macanese Pataca', 'MOP$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Mauritian Rupee', 'MURs');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Malaysian Ringgit', 'RM');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Mozambican Metical', 'MTn');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Namibian Dollar', 'N$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Nigerian Naira', '₦');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Nicaraguan Córdoba', 'C$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Nepalese Rupee', 'नेरू');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Panamanian Balboa', 'B/.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Peruvian Nuevo Sol', 'S/.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Philippine Peso', '₱');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Pakistani Rupee', '₨');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Polish Zloty', 'zł');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Paraguayan Guarani', '₲');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Romanian Leu', 'RON');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Serbian Dinar', 'дин.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Russian Ruble', 'руб.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Rwandan Franc', 'FR');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Sudanese Pound', 'SDG');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Somali Shilling', 'Ssh');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Thai Baht', '฿');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Tongan Paʻanga', 'T$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Turkish Lira', 'TL');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('New Taiwan Dollar', 'NT$');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Tanzanian Shilling', 'TSh');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Ukrainian Hryvnia', '₴');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Ugandan Shilling', 'USh');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Uzbekistan Som', 'UZS');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Venezuelan Bolívar', 'Bs.F.');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Vietnamese Dong', '₫');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('CFA Franc BEAC', 'FCFA');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('CFA Franc BCEAO', 'CFA');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('South African Rand', 'R');
+
+-
+INSERT INTO
+    symbol_currency (name, code)
+VALUES
+    ('Zambian Kwacha', 'ZK');
