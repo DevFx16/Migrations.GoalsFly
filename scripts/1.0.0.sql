6c4ec0355c8fc55caa5f10aa7c6b865eca07155d
Fernando Araujo <developgadget@gmail.com>
2024-11-08 23:20:13 -0500

    Add types

--- scripts/1.0.0.sql
+++ scripts/1.0.0.sql
@@ -4,21 +4,25 @@ CREATE TABLE
         name TEXT,
         value TEXT
     );
+
 -
 INSERT INTO
     configurations (id, name, value)
 VALUES
     (1, 'is_configured', 'false');
+
 -
 INSERT INTO
     configurations (id, name, value)
 VALUES
     (2, 'currency', '');
+
 -
 INSERT INTO
     configurations (id, name, value)
 VALUES
     (3, 'format_currency', '');
+
 -
 CREATE TABLE
     goals_type (
@@ -26,16 +30,31 @@ CREATE TABLE
         name TEXT,
         ln18 TEXT
     );
+
 -
 INSERT INTO
     goals_type (id, name, ln18)
 VALUES
-    (1, 'finance', 'finance');
+    (1, 'savings', 'savings');
+
 -
 INSERT INTO
     goals_type (id, name, ln18)
 VALUES
-    (2, 'check_list', 'checkList');
+    (2, 'spends', 'spends');
+
+-
+INSERT INTO
+    goals_type (id, name, ln18)
+VALUES
+    (3, 'debt', 'debt');
+
+-
+INSERT INTO
+    goals_type (id, name, ln18)
+VALUES
+    (4, 'check_list', 'checkList');
+
 -
 CREATE TABLE
     interest_type (
@@ -43,16 +62,19 @@ CREATE TABLE
         name TEXT,
         ln18 TEXT
     );
+
 -
 INSERT INTO
     interest_type (id, name, ln18)
 VALUES
     (1, 'compound', 'interestCompound');
+
 -
 INSERT INTO
     interest_type (id, name, ln18)
 VALUES
     (2, 'simple', 'interestSimple');
+
 -
 CREATE TABLE
     frequency (
@@ -61,21 +83,25 @@ CREATE TABLE
         ln18 TEXT,
         value INTEGER
     );
+
 -
 INSERT INTO
     frequency (id, name, ln18, value)
 VALUES
     (1, 'daily', 'daily', 1);
+
 -
 INSERT INTO
     frequency (id, name, ln18, value)
 VALUES
     (2, 'weekly', 'weekly', 7);
+
 -
 INSERT INTO
     frequency (id, name, ln18, value)
 VALUES
     (3, 'monthly', 'monthly', 30);
+
 -
 CREATE TABLE
     goals (
@@ -94,7 +120,9 @@ CREATE TABLE
         FOREIGN KEY (type_id) REFERENCES goals_type (id),
         FOREIGN KEY (interest_type_id) REFERENCES interest_type (id)
     );
--
+
+- 
+
 CREATE TRIGGER update_at_goals AFTER
 UPDATE ON goals BEGIN
 UPDATE goals
@@ -102,7 +130,9 @@ SET
     updated_at = datetime ('now')
 WHERE
     id = NEW.id;
+
 END;
+
 -
 CREATE TABLE
     metadata_goals (
@@ -119,7 +149,9 @@ CREATE TABLE
         updated_at TIMESTAMP NULL,
         FOREIGN KEY (goal_id) REFERENCES goals (id)
     );
--
+
+- 
+
 CREATE TRIGGER update_at_metadata_goals AFTER
 UPDATE ON metadata_goals BEGIN
 UPDATE metadata_goals
@@ -127,4 +159,5 @@ SET
     updated_at = datetime ('now')
 WHERE
     id = NEW.id;
+
 END;
